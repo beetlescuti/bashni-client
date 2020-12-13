f0e32829abf14b7e9c302b7a6cd3401daf2ef029
Soren Little <little.soren@campus.lmu.de>
2020-12-13 16:08:46 +0100

    receiving from server

--- performConnection.c
+++ performConnection.c
@@ -1,8 +1,25 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
 
 #include "performConnection.h"
 
-void serverConnect(int socket_file_descriptor) {
-    printf("smth\n");
+int serverConnect(int socket_file_descriptor) {
+
+    char server_msg[1024];
+
+    while(1){
+
+        if(recv(socket_file_descriptor, server_msg, 2000, 0) < 0) {
+            printf("recv failed\n");
+            break;
+        }
+        printf("S: %s", server_msg);
+        break;
+
+    }
+
+    return 0;
 }
--- performConnection.h
+++ performConnection.h
@@ -1,6 +1,6 @@
 #ifndef performConnection
 #define performConnection
 
-void serverConnect(int socket_file_descriptor);
+int serverConnect(int socket_file_descriptor);
 
 #endif
