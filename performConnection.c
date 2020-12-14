2860ad2df8ef0ed670ad712d3015c797a2178331
Soren Little <little.soren@campus.lmu.de>
2020-12-14 20:35:38 +0100

    clean-up, comments for clarity

--- performConnection.c
+++ performConnection.c
@@ -8,67 +8,70 @@
 
 #include "performConnection.h"
 
-// TO-Dos: wrap receive message (with newline as end) into function; continue with implementation of protocol  
+// TODO: wrap receive message (with newline as end) into function
+// TODO: continue with implementation of protocol  
 int serverConnect(int socket_file_descriptor) {
 
-        char server_msg[1024];
-        int result = 0;
-        int packets = 0;
-        int bytes_received= 0;
-        memset(server_msg, 0, 1024);
-        do
-         {
-            result =recv(socket_file_descriptor, &server_msg[bytes_received], 1024-bytes_received, 0);
-            packets++;
-            if (result > 0 )
-            {
-                bytes_received+=result;
-            }
-            else
-            {
-                printf("Error: %i \n", result);
-            }
-            if (bytes_received >= 1024)
-            { printf("Buffer overflow");
-            break;}
-         }
-         while(server_msg[bytes_received-1] != '\n');
+    char server_msg[1024];
+    int result = 0;
+    int packets = 0;
+    int bytes_received= 0;
+    memset(server_msg, 0, 1024);
 
-        //printf(  "S: %s bytes: %i packets: %i \n" , server_msg, bytes_received, packets);
-        printf(  "S: %s" , server_msg);
-        /* send client version:
-          major version must match!! */
-            char client_version[14] = "VERSION 2.3\n";
-            if (send(socket_file_descriptor, client_version, strlen(client_version), 0) < 0) {
-                printf("send failed\n");
-            } else {
-                printf("C: %s", client_version);
-            }
-        result = 0;
-        packets = 0;
-        bytes_received= 0;
-        memset(server_msg, 0, 1024);
-        do
-        {
-            result =recv(socket_file_descriptor, &server_msg[bytes_received], 1024-bytes_received, 0);
-            packets++;
-            if (result > 0 )
-            {
-                bytes_received+=result;
-            }
-            else
-            {
-                printf("Error: %i \n", result);
-            }
-            if (bytes_received >= 1024)
-            { printf("Buffer overflow");
-                break;}
+    /* receive: is server accepting connections? */
+    do {
+        result =recv(socket_file_descriptor, &server_msg[bytes_received], 1024-bytes_received, 0);
+        packets++;
+        if (result > 0 ) {
+            bytes_received+=result;
         }
-        while(server_msg[bytes_received-1] != '\n');
+        else {
+            printf("Error: %i \n", result);
+        }
+        if (bytes_received >= 1024) {
+            printf("Buffer overflow");
+        break;
+        }
+    } while(server_msg[bytes_received-1] != '\n');
 
-        // for debugging only: printf(  "S: %s bytes: %i packets: %i \n" , server_msg, bytes_received, packets);
-        printf(  "S: %s" , server_msg);
+    // for debugging only
+    // printf(  "S: %s bytes: %i packets: %i \n" , server_msg, bytes_received, packets);
+    printf("S: %s" , server_msg);
 
+    /* send: client version, major version must match!! */
+    char client_version[14] = "VERSION 2.3\n";
+    if (send(socket_file_descriptor, client_version, strlen(client_version), 0) < 0) {
+        printf("send failed\n");
+    } 
+    else {
+        printf("C: %s", client_version);
+    }
 
-        return 0;
+    /* empty previous server message */
+    result = 0;
+    packets = 0;
+    bytes_received= 0;
+    memset(server_msg, 0, 1024);
+
+    /* receive: is client version accepted? */
+    do {
+        result =recv(socket_file_descriptor, &server_msg[bytes_received], 1024-bytes_received, 0);
+        packets++;
+        if (result > 0 ) {
+            bytes_received+=result;
+        }
+        else {
+            printf("Error: %i \n", result);
+        }
+        if (bytes_received >= 1024) {
+            printf("Buffer overflow");
+            break;
+        }
+    } while(server_msg[bytes_received-1] != '\n');
+
+    // for debugging only
+    // printf(  "S: %s bytes: %i packets: %i \n" , server_msg, bytes_received, packets);
+    printf("S: %s" , server_msg);
+
+    return 0;
 }
