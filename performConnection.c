586471a59eb9ad13067a8e5880e8b689e201c397
Svea Marie Meyer <svea.meyer@tum.de>
2020-12-14 15:49:43 +0100

    still getting timeout; and sometimes server message gets weiredly overwritten.

--- performConnection.c
+++ performConnection.c
@@ -10,38 +10,58 @@
 
 int serverConnect(int socket_file_descriptor) {
 
-    char server_msg[1024];
+          char server_msg[1024];
+//        char kick_off[10] = "Lets play";
+//        if(send(socket_file_descriptor, kick_off, strlen(kick_off), 0) < 0){
+//            printf("send failed\n");
+//        }
+//        else{
+//            printf("C: %s\n", kick_off);
+//    }
 
-    while(1){
+
+        int message_length = recv(socket_file_descriptor, server_msg, 1024, 0);
+
         /* check if server is accepting connections */
-        if(recv(socket_file_descriptor, server_msg, 1024, 0) < 0) {
-            printf("recv failed\n");
-            break;
+        if (message_length < 0) {
+           printf("recv failed\n");
         }
-        // TODO: do we need sleep? reply is too fast -> could be a different problem
-        usleep(500000);
-        printf("S: %s", server_msg);
+        else {
+          printf("Real server message from buffer: %s \n", server_msg);
+        }
+
+      //  while(strlen(server_msg) <= message_length){
+        //     printf("%lu",strlen(server_msg));
+
+          //   if ((strlen(server_msg)) == (message_length)){
+            //   printf("S: %s real server message", server_msg);
+            //    break; }}
 
         /* send client version:
-           major version must match!! */
-        char client_version[12] = "VERSION 2.1";
-        if(send(socket_file_descriptor, client_version, strlen(client_version), 0) < 0){
-            printf("send failed\n");
-            break;
-        }
-        printf("C: %s\n", client_version);
+          major version must match!! */
+           char client_version[14] = "Version 2.2";
+           if(send(socket_file_descriptor, client_version, strlen(client_version), 0) < 0){
+               printf("send failed\n");
+           }
+           else{
+             //    printf("Client version that we send: %s \n", client_version);
+          }
 
-        // empty server_msg to receive a new reply
+           // empty server_msg to receive a new reply
         memset(server_msg, 0, 1024);
+        printf("Server message should now be empty S: %s - Is it? \n", server_msg);
 
         /* check if client version is accepted */
         if(recv(socket_file_descriptor, server_msg, 1024, 0) < 0) {
             printf("recv failed\n");
-            break;
+       }
+        else {
+            printf("What we receive after sending our Version S: %s", server_msg);
         }
-        printf("S: %s", server_msg);
-        break;
-    }
 
-    return 0;
+        printf("I will now sleep\n");
+        usleep(5000000);
+
+
+        return 0;
 }
